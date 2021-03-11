ad6749494bb31b55fa5527350c88e439fe5ca392
Denis Czaplia <zgibex333@gmail.com>
2021-03-11 18:29:08 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    if (typeof array !== 'undefined' && array.length > 0) {
+        let min = Math.min.apply(null, array)
+        return min;
+    } else {
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+    if (typeof array !== 'undefined' && array.length > 0) {
+        let max = Math.max.apply(null, array)
+        return max;
+    } else {
+        return 0;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (typeof array !== 'undefined' && array.length > 0) {
+        let avg = array.reduce((a, b) => a + b, 0)
+        return avg / array.length;
+    } else {
+        return 0;
+    }
+
 }
